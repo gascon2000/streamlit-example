cea64f351e6615c4cf5cc7f5c050a2e8f32857ef
gascon2000 <83176798+gascon2000@users.noreply.github.com>
2024-01-10 16:06:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,22 +5,12 @@ st.markdown("<h1 style='text-align: center;'>PandaQ</h1>", unsafe_allow_html=Tru
 st.markdown("<p style='text-align: center; font-size: 20px;'>Josep Gascon</p>", unsafe_allow_html=True)
 st.write("")
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-def remote_css(url):
-    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
+st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
 
-def icon(icon_name):
-    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
-
-local_css("style.css")
-remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
-
-icon("search")
-selected = st.text_input("", "Search...")
-button_clicked = st.button("OK")
+selected = st.text_input("", "Select * from countries;")
+button_clicked = st.button("Submit")
 
 
 # Load the data
