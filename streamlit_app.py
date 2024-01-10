c79ba564136d7fd354769333d0522baf3bb741f2
gascon2000 <83176798+gascon2000@users.noreply.github.com>
2024-01-10 14:17:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,8 @@ import base64
 import io
 import os
 
-# Vykreslení tabulky s logy
-st.markdown("<h1 style='text-align: center;'>Žebříček největších společností světa</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; font-size: 20px;'>Podle tržní kapitalizace v miliardách dolarů </p>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>PandaQ</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-size: 20px;'>Josep Gascon</p>", unsafe_allow_html=True)
 st.write("")
 
 def local_css(file_name):
@@ -37,24 +36,6 @@ def load_data():
 
 data = load_data().copy()
 
-# Convert image to Base64
-def image_to_base64(img_path, output_size=(64, 64)):
-    # Check if the image path exists
-    if os.path.exists(img_path):
-        with Image.open(img_path) as img:
-            img = img.resize(output_size)
-            buffered = io.BytesIO()
-            img.save(buffered, format="PNG")
-            return f"data:image/png;base64,{base64.b64encode(buffered.getvalue()).decode()}"
-    return ""
-
-# If 'Logo' column doesn't exist, create one with path to the logos
-if 'Logo' not in data.columns:
-    output_dir = 'downloaded_logos'
-    data['Logo'] = data['Name'].apply(lambda name: os.path.join(output_dir, f'{name}.png'))
-
-# Convert image paths to Base64
-data["Logo"] = data["Logo"].apply(image_to_base64)
 
 nazev_column = st.column_config.TextColumn(label="Název společnosti")
 market_cap_column = st.column_config.TextColumn(label="Tržní kapitalizace 💬",help="📍**v mld. USD**")
@@ -68,12 +49,5 @@ data.index = data.index + 1
 data = data[['Name', 'Market Cap', 'Price']]
 
 
-# Display the dataframe
-st.dataframe(data, height=913, column_config={"Name":nazev_column,'Market Cap':market_cap_column,'Price':price_column})
 
-import datetime
 
-# Získání aktuálního data
-dnesni_datum = datetime.date.today().strftime("%d.%m.%Y")  # Formátování data na formát DD.MM.YYYY
-
-st.markdown(f'<span style="font-size: 14px">**Zdroj:** companiesmarketcap.com | **Data:** k {dnesni_datum} | **Autor:** lig </span>', unsafe_allow_html=True)
