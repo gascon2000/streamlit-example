65692e2cac06080d2b3b8cf1c4b8573137d91202
gascon2000 <83176798+gascon2000@users.noreply.github.com>
2024-01-10 16:10:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,14 @@ def local_css(file_name):
 def remote_css(url):
     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
 
+def icon(icon_name):
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
 
 local_css("style.css")
+remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
-selected = st.text_input("", " ")
+icon("search")
+selected = st.text_input("", "")
 button_clicked = st.button("Submit")
 
 
